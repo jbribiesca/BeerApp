f30499b061a2dacd1cebdf95a23454948a3a0bd1
NiBeCa <nbcald@gmail.com>
2019-05-16 14:05:39 -0500

    added wip-folder for work in progress files, created beer-model based on sql info, renamed files to beer related code

--- server.js
+++ server.js
@@ -13,17 +13,12 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Handlebars
-app.engine(
-  "handlebars",
-  exphbs({
-    defaultLayout: "main"
-  })
-);
+app.engine("handlebars",exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
 // Routes
-require("./routes/apiRoutes")(app);
-require("./routes/htmlRoutes")(app);
+require("./routes/beer-apiRoutes")(app);
+require("./routes/beer-htmlRoutes")(app);
 
 var syncOptions = { force: false };
 
