2cd7d72d547fe149800595ce196bca33276b6efa
NiBeCa <nbcald@gmail.com>
2019-05-17 16:52:22 -0500

    moved post method to javascript file --ajax

--- routes/index-apiRoutes.js
+++ routes/index-apiRoutes.js
@@ -10,14 +10,7 @@ module.exports = function(app) {
 
   // Create a new User
   app.post("/api/users", function(req, res) {
-    db.User.create({
-      birthday: req.body.birthday,
-      first_name: req.body.first_name,
-      last_name: req.body.last_name,
-      email: req.body.email,
-      password: req.body.password,
-      zip: req.body.zip_code
-    }).then(function(dbUsers) {
+    db.User.create(req.body).then(function(dbUsers) {
       res.json(dbUsers);
     });
   });
