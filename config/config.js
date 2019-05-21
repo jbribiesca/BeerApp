160ec527407ff8fd64820e13a137c1ed2c17b105
NiBeCa <nbcald@gmail.com>
2019-05-21 15:13:29 -0500

    changed config file set up to hide mySQL log in

--- config/config.js
+++ config/config.js
@@ -1,9 +1,13 @@
-{
+
+require('dotenv').config(); 
+
+module.exports = {
+
   "development": {
-    "username": "root",
-    "password": "password",
-    "database": "burp_db",
-    "host": "localhost",
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_DATABASE,
+    "host": process.env.DB_HOST,
     "dialect": "mysql"
   },
   "test": {
@@ -18,4 +22,4 @@
     "use_env_variable": "JAWSDB_URL",
     "dialect": "mysql"
   }
-}
+};
