0f81d4353f8d53f176a3d28b902a041cb44b2e45
jbribiesca <bribiesca@gmail.com>
2019-05-22 09:11:03 -0500

    fixed api beer routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,7 +1,29 @@
 var keys = require("../keys.js");
 var axios = require("axios");
+var db = require("../models");
 
 module.exports = function(app) {
+
+   // Get all beers
+   app.get("/api/beers", function(req, res) {
+    db.Beer.findAll({}).then(function(dbbeers) {
+      res.json(dbbeers);
+    });
+  });
+
+  // Create a new Beer
+  app.post("/api/beers", function(req, res) {
+    db.Beer.create(req.body).then(function(dbBeer) {
+      res.json(dbBeer);
+    });
+  });
+
+  // Delete an Beer by id
+  app.delete("/api/beers/:id", function(req, res) {
+    db.Beer.destroy({ where: { id: req.params.id } }).then(function(dbBeer) {
+      res.json(dbBeer);
+    });
+  });
 
 // Third party API - Untapped - for beer search
 
